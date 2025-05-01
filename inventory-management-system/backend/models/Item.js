f340df8e4212a8ff2137e71a17e57e2184bbf075
killer1panda <130059990+killer1panda@users.noreply.github.com>
2025-05-01 19:11:25 +0530

    Delete inventory-management-system directory

--- inventory-management-system/backend/models/Item.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const ItemSchema = new mongoose.Schema({
-  name: String,
-  quantity: Number,
-  supplier: String,
-});
-module.exports = mongoose.model('Item', ItemSchema);
